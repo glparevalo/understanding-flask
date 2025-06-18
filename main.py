7ca91e969e9aa879c05cccf86c7940483ee82dbc
Gabriel Arevalo <lyanearevalo@gmail.com>
2025-06-18 19:16:14 +0800

    added line

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# This makes it possible for us to run main.py as a script
+if __name__ == "__main__":
+    app.run()
